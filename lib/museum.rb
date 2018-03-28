e93d678c3ce7ba8cc0a5b3634bb7af07b66a5a62
Tyler Westlie <tyler.westlie@gmail.com>
2018-03-28 09:51:46 -0600

    finished iteration 1

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,15 @@
 require 'pry'
 class Museum
 
-  attr_reader :museum
+  attr_reader :museum, :exhibits
 
   def initialize(museum)
     @museum = museum
+    @exhibits = Hash.new
   end
-  
+
+  def add_exhibit(name, cost)
+    @exhibits[name] = cost
+  end
+
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -4,8 +4,19 @@ require './lib/museum'
 require 'pry'
 
 class TestMuseum < Minitest::Test
+
   def test_exists
     dmns = Museum.new("Denver Museum of Natural Science")
     assert_instance_of Museum, dmns
   end
+
+  def test_add_exhibits
+    dmns = Museum.new("Denver Museum of Natural Science")
+    dmns.add_exhibit("Dead Sea Scrolls", 10)
+    dmns.add_exhibit("Gems and Minerals", 0)
+    a = dmns.exhibits
+
+    assert_equal a, dmns.exhibits
+  end
+
 end
